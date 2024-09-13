9e69ce3c7f7c3b05e4a0ef1114f3b0d9229bcd29
Portillo17e <eduardo2portillo789@gmail.com>
2024-09-13 17:38:15 -0600

    ejercicio 3

---
+++ 3.ts
@@ -0,0 +1,12 @@
+//Eduardo Danilo Rodriguez Portillo
+//1646323
+
+let edad: number = 18;
+
+if (edad < 18) {
+    console.log("Eres menor de edad.");
+} else if (edad >= 18 && edad < 65) {
+    console.log("Eres adulto.");
+} else {
+    console.log("Eres mayor.");
+}
