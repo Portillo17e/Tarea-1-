73e9a4fb9c3d2d691d8f65b926160a8b690e1baf
Portillo17e <eduardo2portillo789@gmail.com>
2024-09-13 17:36:48 -0600

    ejercicio 2

---
+++ 2.ts
@@ -0,0 +1,11 @@
+//Eduardo Danilo Rodriguez Portillo
+//1646323
+
+let a: number = 10;
+let b: number = 5;
+
+console.log("Suma: ", a + b);
+console.log("Resta: ", a - b);
+console.log("Multiplicación: ", a * b);
+console.log("División: ", a / b);
+console.log("Módulo: ", a % b);
