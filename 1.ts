d48a7899cfdf92479ef297d251addf3d3ce627ec
Portillo17e <eduardo2portillo789@gmail.com>
2024-09-13 17:34:24 -0600

    ejercicio 1

---
+++ 1.ts
@@ -0,0 +1,11 @@
+//Eduardo Danilo Rodriguez Portillo 
+//1646323 
+
+
+let numero: number = 25;
+let texto: string = "Hola, TypeScript!";
+let booleano: boolean = true;
+
+console.log(numero);
+console.log(texto);
+console.log(booleano);
