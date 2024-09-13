9f23417dd1956654a4706943226cc5fbbfa31748
Portillo17e <eduardo2portillo789@gmail.com>
2024-09-13 17:41:40 -0600

    ejercicio 4

---
+++ 4.ts
@@ -0,0 +1,14 @@
+//Eduardo Danilo Rodriguez Portillo
+//1646323
+
+console.log("Bucle for:");
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+console.log("Bucle while:");
+let j = 0;
+while (j < 5) {
+    console.log(j);
+    j++;
+}
